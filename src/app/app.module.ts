fb249f87d7e1b41d6125cd711d77e0b9a3083372
Aldwin Tablante <aldwinotablante@gmail.com>
2017-09-16 11:08:50 +0800

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/Forms';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { EntryComponent } from './entry/entry.component';
-import { DisplayComponent } from './display/display.component';
-import {SharedService} from './shared.service';
-import { Routes , RouterModule} from '@angular/router';
-const navRoutes: Routes = [
-		{path: 'entry' , component: EntryComponent},
-		{path: 'display' , component: DisplayComponent}
-  ]
-  
+
 @NgModule({
   declarations: [
-    AppComponent,
-    EntryComponent,
-    DisplayComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(navRoutes),
     FormsModule
   ],
-  providers: [SharedService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
