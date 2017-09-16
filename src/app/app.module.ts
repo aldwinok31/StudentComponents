559482ccc01c494cb68295ebe94f2030fe12bdf8
Aldwin Tablante <aldwinotablante@gmail.com>
2017-09-16 11:15:06 +0800

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { EntryComponent } from './entry/entry.component';
+import { AddEntryComponent } from './entry/add-entry/add-entry.component';
+import { ListEntryComponent } from './entry/add-entry/list-entry/list-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryComponent,
+    AddEntryComponent,
+    ListEntryComponent
   ],
   imports: [
     BrowserModule,
